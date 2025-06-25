05379d855d553b81d5b0c920e7b90c74d6f1f52c
Paraga2003 <153626230+Paraga2003@users.noreply.github.com>
2025-06-25 15:42:27 +0530

    Time: 20 ms (51.34%) | Memory: 19.2 MB (65.67%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -13,27 +13,39 @@ public:
         while (j >= 0 && directions[j] == 'R') {
             j--;
         }
-        // while(i<n){
-        //     while(directions[i]=='R'){
-        //         tempCount++;
-        //         i++;
-        //         continue;
-        //     }
-        //     if(directions[i]=='S'){
-        //             ans+=tempCount;
-        //     }else{
-        //             ans= ans+ tempCount+1;
-        //     }
-        //     tempCount = 0;
-        //     i++;
-
-        // }
-        // return ans;
-        for (int k = i; k <= j; k++) {
-            if (directions[k] != 'S') {
-                ans++;
+        while (i < n) {
+            while (i < n && directions[i] == 'R') {
+                tempCount++;
+                i++;
+            }
+            
+            if (i < n && directions[i] == 'S') {
+                ans += tempCount;
+                tempCount = 0;
+                i++;
+            } else if (i < n && directions[i] == 'L') {
+                
+                ans += tempCount + 1; 
+                tempCount = 0;
+                i++;
+                
+                
+                while (i < n && directions[i] == 'L') {
+                    ans++;
+                    i++;
+                }
+            } else {
+                
+                tempCount = 0;
+                i++;
             }
         }
+       
+        // for (int k = i; k <= j; k++) {
+        //     if (directions[k] != 'S') {
+        //         ans++;
+        //     }
+        // }
         
         return ans;
         
