fa3ee36116230f194823733b1490abcc3a24b500
Paraga2003 <153626230+Paraga2003@users.noreply.github.com>
2025-07-05 15:41:34 +0530

    Time: 35 ms (69.55%) | Memory: 32.3 MB (38.71%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n=hand.size();
+        
+        int n = hand.size();
         if(n%groupSize!=0){
             return false;
         }
-        map<int,int> cardCtrMap;
-        for(int &handno:hand){
-            cardCtrMap[handno]++;
+        map<int,int> cardmap;
+        for(int i:hand){
+            cardmap[i]++;
         }
-        
-        while(!cardCtrMap.empty()){
-            
-            int curr=cardCtrMap.begin()->first;
-            
+        while(!cardmap.empty()){
+            int curr = cardmap.begin()->first;
+
             for(int i=0;i<groupSize;i++){
-                if(cardCtrMap[curr+i]==0){
+                if(cardmap[curr+i]==0){
                     return false;
                 }
-                cardCtrMap[curr+i]--;
-                if(cardCtrMap[curr+i]<1){
-                    cardCtrMap.erase(curr+i);
+                cardmap[curr+i]--;
+                if(cardmap[curr+i]<1){
+                    cardmap.erase(curr+i);
                 }
             }
         }
         return true;
-
-        
     }
 };
